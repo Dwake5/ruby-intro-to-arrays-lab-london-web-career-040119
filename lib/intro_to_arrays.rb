15d07a583971279b4f3bd95d031b0c752f3de8a1
Danny Wakeling <wakeling101@hotmail.com>
2019-03-02 12:25:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,19 +3,19 @@ def instantiate_new_array
 end
 
 def array_with_two_elements
-  return [ 1, 2]
+  return [1, 2]
 end
 
-def first_element
-
+def first_element(array)
+  return array.[0]
 end
 
-def third_element
-
+def third_element(array)
+  return array.[2]
 end
 
 def last_element
-
+  return array.last
 end
 
 def first_element_with_array_methods
